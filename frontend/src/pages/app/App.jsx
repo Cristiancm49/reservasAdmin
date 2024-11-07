0262730d871935bd35f52b943b5d3a0a6676364d
ccortesm49 <ccortesm2000@gmail.com>
2024-11-07 12:03:17 -0500

    se crea carpeta frontend y se inica el proyecto en react

---
+++ frontend/src/pages/app/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from '../login';
+
+import './App.css'
+
+function App  ()  {
+  
+
+  return (
+    <Router>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  </Router>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/login/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+  return(
+    <div className='w-screen h-screen flex justify-center items-center'>
+        <div>
+            <p>login</p>
+        </div>
+    </div>
+  );
+};
+
+export default Home;
